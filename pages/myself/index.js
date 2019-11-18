878509db0dcf214e0d554e8377a4b4ebd6b7d3d6
刘正 <itliuzheng@sina.com>
2019-11-18 19:33:43 +0800

    first commit

---
+++ pages/myself/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    icon: '../../images/myself/list_icon1.png',
+  },
+  //事件处理函数
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  onLoad: function () {
+  },
+})
