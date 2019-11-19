fddd47b887d98740f80999aa4c01aa70182f458e
刘正 <itliuzheng@sina.com>
2019-11-19 17:25:42 +0800

    modify

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  api_base_url: 'http://119.147.171.111:81/',
+  uploadImageUrl: 'http://119.147.171.111:81/',
+  AccessKeySecret: '******',
+  OSSAccessKeyId: '******',
+  timeout: 80000 //上传文件时Policy的失效时间
+}
+export {
+  config
+}
--- pages/myself/index.js
+++ pages/myself/index.js
@@ -4,7 +4,6 @@ const app = getApp()
 
 Page({
   data: {
-    icon: '../../images/myself/list_icon1.png',
   },
   //事件处理函数
   // bindViewTap: function() {
